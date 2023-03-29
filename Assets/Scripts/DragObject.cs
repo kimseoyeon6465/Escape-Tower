167fb5d08efa783270dcaa94b1620ab3d2c526d7
kimseoyeon6465 <48300652+kimseoyeon6465@users.noreply.github.com>
2023-03-29 23:52:06 +0900

    Tree, Axe Move
    By dragging mouse

---
+++ Assets/Scripts/DragObject.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DragObject : MonoBehaviour//안움직임
+{
+    private bool isDragging = false;
+    private float distance;
+
+    void OnMouseDown()
+    {
+        isDragging = true;
+        //distance = Vector3.Distance(transform.position, Camera.main.transform.position);
+    }
+    
+    void OnMouseUp()
+    {
+        isDragging = false;
+    }
+    
+    void Update()
+    {
+        if (isDragging)
+        {
+            Debug.Log(isDragging);
+            //Vector3 mousePosition = new Vector3(Input.mousePosition.x, Input.mousePosition.y, distance);
+            Vector3 mousePosition = new Vector3(Input.mousePosition.x, Input.mousePosition.y, Input.mousePosition.z);
+            Vector3 worldPosition = Camera.main.ScreenToWorldPoint(mousePosition);
+            transform.position = worldPosition;
+            print(worldPosition);
+        }
+    }
+    
+}
+
