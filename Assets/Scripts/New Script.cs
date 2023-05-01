9514b1990ebdaf841ca62b26f7e8e5bc708636e1
kimseoyeon6465 <48300652+kimseoyeon6465@users.noreply.github.com>
2023-05-01 23:50:29 +0900

    Add New Script

---
+++ Assets/Scripts/New Script.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        //새로운 스크립트
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
