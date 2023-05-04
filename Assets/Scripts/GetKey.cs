58124fbb2d485872de6db4a3f6a901ca9cd81f60
kimseoyeon6465 <48300652+kimseoyeon6465@users.noreply.github.com>
2023-05-04 22:23:45 +0900

    Add GetKey Function

---
+++ Assets/Scripts/GetKey.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GetKey : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    private void OnMouseDown()//열쇠를 클릭하면 열쇠 카운트 증가, 열쇠 갯수 채우면 게임 종료
+    {
+        
+    }
+}
