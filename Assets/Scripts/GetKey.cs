5a926c51af6f4d3c2d5dd47197012ff3055b108b
kimseoyeon6465 <48300652+kimseoyeon6465@users.noreply.github.com>
2023-05-05 23:29:28 +0900

    GetKey System

--- Assets/Scripts/GetKey.cs
+++ Assets/Scripts/GetKey.cs
@@ -1,13 +1,16 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class GetKey : MonoBehaviour
 {
+    public Text text;
+    int keyCount = 0;
     // Start is called before the first frame update
     void Start()
     {
-        
+        SetText();
     }
 
     // Update is called once per frame
@@ -15,8 +18,21 @@ public class GetKey : MonoBehaviour
     {
         
     }
-    private void OnMouseDown()//열쇠를 클릭하면 열쇠 카운트 증가, 열쇠 갯수 채우면 게임 종료
+    void OnMouseDown()//열쇠를 클릭하면 열쇠 카운트 증가, 열쇠 갯수 채우면 게임 종료
     {
-        
+        Debug.Log("key is clicked");
+        GetScore();
+        Destroy(this.gameObject);
+    }
+    public void GetScore()
+    {
+        keyCount += 1;
+        Debug.Log(keyCount);
+        SetText();
+    }
+
+    public void SetText()
+    {
+        text.text = "Key : " + keyCount.ToString();
     }
 }
---
+++ Assets/Scripts/MenuButton.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuButton : MonoBehaviour
+{
+    public GameObject menuPanel;
+    // Start is called before the first frame update
+    void Start()
+    {
+        menuPanel.SetActive(false);
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public void MenuButtonClicked()
+    {
+        Debug.Log("Button Clicked");
+        menuPanel.SetActive(true);
+    }
+}
