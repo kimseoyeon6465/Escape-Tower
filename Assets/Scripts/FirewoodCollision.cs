bc6a63149f54aa20755431824580994316eb83bc
kimseoyeon6465 <48300652+kimseoyeon6465@users.noreply.github.com>
2023-05-12 23:47:57 +0900

    StartCoroutine

--- Assets/Scripts/FirewoodCollision.cs
+++ Assets/Scripts/FirewoodCollision.cs
@@ -7,12 +7,13 @@ public class FirewoodCollision : MonoBehaviour
     public GameObject FirewoodObject;
     public GameObject FirePrefab;
     public GameObject SnowMan;
+    private float targetScale = 0.25f;
+    private float duration = 2f;
     // Start is called before the first frame update
     void Start()
     {
-        //FirewoodObject = GameObject.Find("Firewood_B(Clone)");
+        FirewoodObject = GameObject.Find("Firewood_B(Clone)");
         SnowMan=GameObject.FindWithTag("Snowman");
-
     }
 
     // Update is called once per frame
@@ -29,12 +30,9 @@ public class FirewoodCollision : MonoBehaviour
             GameObject fire = Instantiate(FirePrefab, FirewoodPosition, Quaternion.identity);
             //여기서 Destroy를 해야하지 않나?
             //Destroy(객체);
-            Invoke("DestroySnowMan", 3f);
-            //뭘해야할지 모르겠음
-            //여전히 뭘 해야할지 모르겠음
-            //아직도 뭘 해야할지 모르겠음
-            //오늘도 뭘 해야할지 모르겠음
-            //Destroy(this.gameObject);
+            //Invoke("DestroySnowMan", 3f);
+            //Invoke("ResizeSnowman", 3f);
+            StartCoroutine(ResizeObjectOverTime(SnowMan,targetScale, duration));
 
         }
     }
@@ -42,5 +40,29 @@ public class FirewoodCollision : MonoBehaviour
     {
         Destroy(SnowMan);
         Debug.Log("3초 지나서 호출");
+    }
+    void ResizeSnowman()
+    {
+        SnowMan.transform.localScale *= 0.8f;
+
+    }
+    public IEnumerator ResizeObjectOverTime(GameObject targetObject, float targetScale, float duration)
+    {
+        Debug.Log("코루틴 호출");
+        Transform objectTransform = targetObject.GetComponent<Transform>();
+        Vector3 initialScale = objectTransform.localScale;
+        Vector3 targetScaleVector = new Vector3(targetScale, targetScale, targetScale);
+
+        float elapsedTime = 0f;
+        while (elapsedTime < duration)
+        {
+            Debug.Log("while문 걸림");
+            objectTransform.localScale = Vector3.Lerp(initialScale, targetScaleVector, elapsedTime / duration);
+            elapsedTime += Time.deltaTime;
+            yield return null;
+        }
+
+        objectTransform.localScale = targetScaleVector; // 최종 크기로 설정
+        yield return new WaitForSeconds(3f);
     }
 }
