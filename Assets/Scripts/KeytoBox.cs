df24e44c1672b44962dfa71a4d0b41c501d885c6
kimseoyeon6465 <48300652+kimseoyeon6465@users.noreply.github.com>
2023-05-18 23:20:09 +0900

    Mini Map Fix

--- Assets/Scripts/KeytoBox.cs
+++ Assets/Scripts/KeytoBox.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class KeytoBox : MonoBehaviour
 {
@@ -8,6 +9,11 @@ public class KeytoBox : MonoBehaviour
     public GameObject ClosedBoxObject;
     public GameObject OpenBoxPrefab;
     public GameObject openbox;
+    AudioSource audioSource;
+    void Awake()
+    {
+        audioSource = this.gameObject.GetComponent<AudioSource>();
+    }
     void Start()
     {
         ClosedBoxObject = GameObject.Find("box10");
@@ -23,6 +29,7 @@ public class KeytoBox : MonoBehaviour
         if(other.gameObject.CompareTag("Box"))
         {
             Debug.Log("box key collision");
+            this.audioSource.Play();
             Vector3 BoxPosition = ClosedBoxObject.transform.position;
             openbox = Instantiate(OpenBoxPrefab, BoxPosition, Quaternion.Euler(0,180.0f,0));
             //openbox = Instantiate(OpenBoxPrefab, BoxPosition, Quaternion.identity);
