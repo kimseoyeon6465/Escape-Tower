4e93d5788d2d72dececccf636ec4321a07d59a70
kimseoyeon6465 <48300652+kimseoyeon6465@users.noreply.github.com>
2023-06-01 23:35:17 +0900

    Update FPSCameraTurn.cs

--- Assets/Scripts/FPSCameraTurn.cs
+++ Assets/Scripts/FPSCameraTurn.cs
@@ -28,15 +28,12 @@ public class FPSCameraTurn : MonoBehaviour
     {
 
         float yRotateSize = Input.GetAxis("Mouse X") * turnSpeed;
-        // 현재 y축 회전값에 더한 새로운 회전각도 계산
         float yRotate = transform.eulerAngles.y + yRotateSize;
 
-        // 위아래로 움직인 마우스의 이동량 * 속도에 따라 카메라가 회전할 양 계산(하늘, 바닥을 바라보는 동작)
         float xRotateSize = -Input.GetAxis("Mouse Y") * turnSpeed;
 
         xRotate = Mathf.Clamp(xRotate + xRotateSize, -45, 80);
 
-        // 카메라 회전량을 카메라에 반영(X, Y축만 회전)
         transform.eulerAngles = new Vector3(xRotate, yRotate, 0);
     }
 }
